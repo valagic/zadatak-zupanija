2f71a772de6ebd7435c71e2ff4def7f41c17e1f3
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-05-14 15:09:08 +0200

    Update zupanija.sql
    napravljen insert,select,update,delete

--- zupanija.sql
+++ zupanija.sql
@@ -29,3 +29,49 @@ create table zupan(
 alter table opcina add foreign key(zupanija) references zupanija(sifra);
 alter table mjesto add foreign key(opcina) references opcina(sifra);
 alter table zupanija add foreign key(zupan) references zupan(sifra);
+
+insert into zupan(ime,prezime) values
+('Ivan','Anušiæ'),('Božo','Galiæ'),('Danijel','Marušiæ');
+select * from zupan;
+
+insert into zupanija(naziv,zupan) values
+('Osjeèko-baranjska',1),('Vukovarsko-srijemska',2),('Brodsko-posavska',3);
+select * from zupanija;
+
+insert into opcina(zupanija,naziv) values
+(1,'Antunovac'),(1,'Bilje'),(2,'Drenovci'),(2,'Cerna'),(3,'Bebrina'),(3,'Brodski Stupnik');
+select * from opcina;
+
+insert into mjesto(opcina,naziv) values
+(1,'Antunovac'),(1,'Ivanovac'),(2,'Bilje'),(2,'Kopaèevo'),(3,'Drenovci'),(3,'Rajevo selo'),(2,'Cerna'),(2,'Šiškovci'),(3,'Bebrina'),(3,'Banovci'),(3,'Brodski Stupnik'),(3,'Stari Slatinik');
+select * from mjesto;
+
+UPDATE mjesto
+SET naziv = 'AntunovacNovo'
+where sifra=1;
+UPDATE mjesto
+SET naziv = 'IvanovacNovo'
+where sifra=2;
+UPDATE mjesto
+SET naziv = 'BiljeNovo'
+where sifra=3;
+UPDATE mjesto
+SET naziv = 'KopaèevoNovo'
+where sifra=4;
+UPDATE mjesto
+SET naziv = 'DrenovciNovo'
+where sifra=5;
+
+delete from opcina where sifra=4;
+delete from opcina where sifra=5;
+
+
+
+
+
+
+
+
+
+
+
